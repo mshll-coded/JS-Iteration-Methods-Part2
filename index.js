07bb46c6ce1a6136a77c9686bc25f7430f9d4b6a
meshal <imeshalfm@gmail.com>
2024-10-08 14:06:53 +0300

    complete task

---
+++ index.js
@@ -0,0 +1,59 @@
+// You are given an array representing a series of transactions in a small business.
+// Each transaction is an array containing the transaction type ("income" or "expense") and the amount.
+const transactions = [
+  ['income', 1000],
+  ['income', 1500],
+  ['expense', 500],
+  ['expense', 300],
+  ['income', 700],
+];
+
+/*****************************************************************
+Part 1: Transaction Analysis
+
+Task 1) Create a new array containing only the income transactions.
+       Output 1) [["income", 1000], ["income", 1500], ["income", 700]]
+
+Task 2) Create a new array containing only the expense transactions.
+       Output 2) [["expense", 500], ["expense", 300]]
+
+******************************************************************/
+
+const incomeTransactions = transactions.filter((transaction) => transaction[0] === 'income');
+console.log(incomeTransactions);
+
+const expenseTransactions = transactions.filter((transaction) => transaction[0] === 'expense');
+console.log(expenseTransactions);
+
+/*****************************************************************
+Part 2: Financial Summary
+
+Task 3) Calculate the total income and return it.
+       Output 3) 3200
+
+Task 4) Calculate the total expenses and return it.
+       Output 4) 800
+
+******************************************************************/
+const totalIncome = incomeTransactions.reduce((total, transaction) => (transaction[0] === 'income' ? total + transaction[1] : total), 0);
+console.log(totalIncome);
+
+const totalExpenses = expenseTransactions.reduce((total, transaction) => (transaction[0] === 'expense' ? total + transaction[1] : total), 0);
+console.log(totalExpenses);
+
+/*****************************************************************
+Part 3: Net Analysis
+
+Task 5) Calculate the net total (total income - total expenses) and return it.
+       Output 5) 2400
+
+Task 6) Identify and create a new array with transactions (both incomes and expenses) above  or equal to $500.
+       Output 6) [["income", 1000], ["income", 1500], ["expense", 500], ["income", 700]]
+
+******************************************************************/
+
+const netTotal = totalIncome - totalExpenses;
+console.log(netTotal);
+
+const transactionsOver500 = transactions.filter((transaction) => transaction[1] >= 500);
+console.log(transactionsOver500);
